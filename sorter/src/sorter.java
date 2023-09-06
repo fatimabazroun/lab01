8cadc0ec5f6e5afb546d5486de7554dc5fa92fde
Fatima <fatimabazroun@gmail.com>
2023-09-06 21:02:46 +0300

    added the selection sort

--- sorter/src/sorter.java
+++ sorter/src/sorter.java
@@ -1,5 +1,25 @@
-public class Main {
+public class sorter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array1 = {9,14,3,2,43,11,58,22,7,5};
+        selectionSort(array1);
+        for(int i:array1){
+            System.out.print(i+" ");
+        }
+
     }
+
+    public static void selectionSort(int[] Array) {
+        for (int i = 0; i < Array.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < Array.length; j++) {
+                if (Array[j] < Array[index]) {
+                    index = j;
+                }
+            }
+            int smallerNumber = Array[index];
+            Array[index] = Array[i];
+            Array[i] = smallerNumber;
+        }
+    }
+
 }
